aa0296c248e9cbe0325b8223d8b2719ae65f0fbf
Maryna Kravchuk <marinalinkova02@gmail.com>
2025-05-29 14:36:58 +0300

    Updates

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.getElementById('oddity-btn').addEventListener('click', function () {
   const logo = document.getElementById('google-logo');
   if (logo) {
-    logo.src = '/src/img/logo.svg';
+    logo.src = '/public/img/logo.svg';
     logo.alt = 'Oddity';
   }
 });
